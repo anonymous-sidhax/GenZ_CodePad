5a5a0ea4453186daa1d3265b642e8f695145937d
siddeshshewde <siddesh.shewde@gmail.com>
2021-10-25 05:24:06 -0400

    Update urls.py

--- GenZ_CodePad/urls.py
+++ GenZ_CodePad/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('mainApp/', include('mainApp.urls')),
     path('admin/', admin.site.urls),
+    path('', include('mainApp.urls')),
 ]

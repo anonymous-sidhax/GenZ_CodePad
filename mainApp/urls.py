10e37ec68feef17b30698c4542aa4a58408751a9
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-30 02:15:26 -0500

    Python Code Execute + Output
    Attempt #1

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
-from .views import index, about_us
+from .views import index, about_us, runcode
 
 urlpatterns = [
     path('', index, name='homepage'),
     path('about_us/', about_us, name='AboutUs'),
+    path('runcode', runcode, name="runcode"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,11 +1,44 @@
 from django.shortcuts import render
 
+import sys
+
+# def index(request):
+#     while(1):
+#         return render(request, "index.html", {
+#             'code': 'Printing Text to Speech Output here',
+#         })
+#     return render(request, "index.html")
+
 def index(request):
-    while(1):
-        return render(request, "index.html", {
-            'code': 'Printing Text to Speech Output here',
-        })
     return render(request, "index.html")
-    
+
 def about_us(request):
     return render(request, "about_us.html")
+
+def runcode(request):
+
+    if request.method == "POST":
+        code = request.POST['codearea']
+
+        try:
+
+            original_stdout = sys.stdout
+            sys.stdout = open('output_files\\file.txt', 'w')
+
+            # Execute Code
+            exec(code)
+
+            sys.stdout.close()
+
+            # Reset the standard output to its original value
+            sys.stdout = original_stdout
+
+            # Read output from file and save in output variable
+            output = open('output_files\\file.txt', 'r').read()
+
+        except Exception as e:
+            sys.stdout = original_stdout
+            output = e
+
+    # Return and render index page and send codedata and output to show on page
+    return render(request , 'index.html', {"code":code , "output":output})

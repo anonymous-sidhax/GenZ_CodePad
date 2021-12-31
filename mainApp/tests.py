679140e1db8bee344697358e2b1a0fd3df41f80f
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-31 02:57:03 -0500

    Added delete line speech command

--- mainApp/tests.py
+++ mainApp/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+import speech_recognition as sr
 
-# Create your tests here.
+keywords = {
+    "delete" : "delete_line"
+}
+
+class AnalyzeTextToCode:
+    def analyze(speech):
+        if speech in keywords.keys():
+            return getattr(AnalyzeTextToCode, keywords[speech])(script_code)
+
+    def delete_line(script_code):
+        position_of_next_line = script_code.rfind('\n')
+        if (position_of_next_line > 0):
+            script_code = script_code[ : position_of_next_line] + "\n"
+        return script_code
+
+class SpeechToTextEngine:
+    
+    def recognize_voice():
+        mic = sr.Microphone()
+        recognize  = sr.Recognizer()
+
+        while True:
+            with mic as source:
+                recognize.adjust_for_ambient_noise(source)
+                audio = recognize.listen(source)
+
+            try:
+                transcript = recognize.recognize_google(audio)
+                print (transcript)
+                #logging.info(transcript)
+            except sr.RequestError:
+                print ('API Error')
+                continue
+            except sr.UnknownValueError:
+                print ('Unable to recognize speech')
+                print ('Looks like you are smarter than the Computer.!!! 🧠')
+                continue  
+
+#SpeechToTextEngine.recognize_voice()
+
+script_code = "print(\"Hello World\")\nprint(\"Hello World\")\nprint(\"Hello World\")"
+speech = "delete"
+
+print(AnalyzeTextToCode.analyze(speech))

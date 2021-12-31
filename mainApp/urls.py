53e19504848b77b55a7074ceb13d6371967244fb
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-31 03:06:15 -0500

    Minor Changes for Merging

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
-from .views import index, about_us, runcode
+from .views import index, about_us, runcode, sst
 
 urlpatterns = [
     path('', index, name='homepage'),
     path('about_us/', about_us, name='AboutUs'),
     path('runcode', runcode, name="runcode"),
+    path('sst', sst, name="sst"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

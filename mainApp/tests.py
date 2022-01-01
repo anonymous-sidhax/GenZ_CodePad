153d09a101a4b27a612af625f0d41dc632fef2c5
siddeshshewde <siddesh.shewde@gmail.com>
2022-01-01 01:54:53 -0500

    Add New Line from Speech

--- mainApp/tests.py
+++ mainApp/tests.py
@@ -1,26 +1,28 @@
 import speech_recognition as sr
+from word2number import w2n
 
 keywords = {
     "delete all" : "delete_all",
     "delete line" : "delete_line",
-    "delete" : "delete"
+    "delete" : "delete",
+    "new line" : "new_line"
 }
 
-number_map = {
-    "zero" : 0,
-    "one" : 1,
-    "two" : 2,
-    "three" : 3,
-    "four" : 4,
-    "five" : 5,
-    "siz" : 6,
-    "seven" : 7,
-    "eight" : 8,
-    "nine" : 9,
-}
+# number_map = {
+#     "zero" : 0,
+#     "one" : 1,
+#     "two" : 2,
+#     "three" : 3,
+#     "four" : 4,
+#     "five" : 5,
+#     "six" : 6,
+#     "seven" : 7,
+#     "eight" : 8,
+#     "nine" : 9,
+# }
 
 class AnalyzeTextToCode:
-    def analyze(speech):
+    def analyze(script_code, speech):
         for keyword in keywords.keys():
             if keyword in speech:
                 return getattr(AnalyzeTextToCode, keywords[keyword])(script_code, speech)
@@ -33,7 +35,8 @@ class AnalyzeTextToCode:
                     line_number = int(speech[position_of_text_number])
             except:
                 number_in_text = speech[position_of_text_number : ]
-                line_number = number_map[number_in_text]
+                #line_number = number_map[number_in_text]
+                line_number = w2n.word_to_num(number_in_text)
             script_code = script_code.split("\n")
             if len(script_code) < line_number:
                 print("Error: Line Number to delete exceeds the total lines in code.")
@@ -52,6 +55,10 @@ class AnalyzeTextToCode:
     
     def delete(script_code, speech):
         script_code = script_code[ : len(script_code)-1]
+        return script_code
+
+    def new_line(script_code, speech):
+        script_code = script_code + '\n'
         return script_code
 
 class SpeechToTextEngine:
@@ -81,6 +88,7 @@ class SpeechToTextEngine:
 
 
 script_code = "print(\"Hello World1\")\nprint(\"Hello World2\")\nprint(\"Hello World3\")\nprint(\"Hello World4\")\nprint(\"Hello World5\")\nprint(\"Hello World6\")"
-speech = SpeechToTextEngine.recognize_voice()
+#speech = SpeechToTextEngine.recognize_voice()
+speech = "add new line"
 
-print(AnalyzeTextToCode.analyze(speech))
+print(AnalyzeTextToCode.analyze(script_code, speech))

12233d58acfffbb40b296b6add52bc5fcef7c64d
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-31 19:19:17 -0500

    Modifications in Delete Speech
    Added:
    - Delete All Code.
    - Delete Current Line.
    - Delete Current Alphabet.

--- mainApp/tests.py
+++ mainApp/tests.py
@@ -1,18 +1,34 @@
 import speech_recognition as sr
 
 keywords = {
-    "delete" : "delete_line"
+    "delete all" : "delete_all",
+    "delete line" : "delete_line",
+    "delete" : "delete"
 }
 
 class AnalyzeTextToCode:
     def analyze(speech):
-        if speech in keywords.keys():
-            return getattr(AnalyzeTextToCode, keywords[speech])(script_code)
+        for keyword in keywords.keys():
+            if keyword in speech:
+                return getattr(AnalyzeTextToCode, keywords[keyword])(script_code, speech)
 
-    def delete_line(script_code):
-        position_of_next_line = script_code.rfind('\n')
-        if (position_of_next_line > 0):
-            script_code = script_code[ : position_of_next_line] + "\n"
+    def delete_line(script_code, speech):
+        if "number" in speech:
+            position_of_number = script_code.rfind('number') + len("number")
+            print (position_of_number)
+            return speech[position_of_number + 1]
+        else:
+            position_of_next_line = script_code.rfind('\n')
+            if (position_of_next_line > 0):
+                script_code = script_code[ : position_of_next_line] + "\n"
+            return script_code
+    
+    def delete_all(script_code, speech):
+        script_code = ""
+        return script_code
+    
+    def delete(script_code, speech):
+        script_code = script_code[ : len(script_code)-1]
         return script_code
 
 class SpeechToTextEngine:
@@ -29,6 +45,7 @@ class SpeechToTextEngine:
             try:
                 transcript = recognize.recognize_google(audio)
                 print (transcript)
+                return transcript
                 #logging.info(transcript)
             except sr.RequestError:
                 print ('API Error')
@@ -38,9 +55,9 @@ class SpeechToTextEngine:
                 print ('Looks like you are smarter than the Computer.!!! 🧠')
                 continue  
 
-#SpeechToTextEngine.recognize_voice()
+
 
 script_code = "print(\"Hello World\")\nprint(\"Hello World\")\nprint(\"Hello World\")"
-speech = "delete"
+speech = SpeechToTextEngine.recognize_voice()
 
 print(AnalyzeTextToCode.analyze(speech))
